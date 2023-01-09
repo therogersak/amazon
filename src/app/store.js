f025455e66e0a258b71a6aaf2abaf1d00a0526de
therogersak <therogersak@gmail.com>
2023-01-09 13:25:35 +0530

    ✨ persist store

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,35 @@
-import { configureStore, createReducer } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  createReducer,
+} from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { productApi } from "../services/productApi";
 import cartReducer from "../features/CartSlice";
 import searchReducer from "../features/SearchSlice";
 import { commentApi } from "../services/commentApi";
 
+// combineReducers
+const reducers = combineReducers({
+  [productApi.reducerPath]: productApi.reducer,
+  [commentApi.reducerPath]: commentApi.reducer,
+  cart: cartReducer,
+  search: searchReducer,
+});
+
+// persist store config
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// persist reducer config
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    [productApi.reducerPath]: productApi.reducer,
-    [commentApi.reducerPath]: commentApi.reducer,
-    cart: cartReducer,
-    search: searchReducer,
-  },
+  reducer: persistedReducer,
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
